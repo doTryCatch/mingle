ea45e94cd5a35c958a8761ee93394e35706e3c0e
Vibhaeo Mudia <VibhaeoM01>
2024-09-02 18:01:49 +0530

    update:page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image src={Logo} alt="logo image" width={200} height={200} />
       <h1>This is the main page after login</h1>
-      <h2>HELLO</h2>
+      
     </main>
   );
 }
